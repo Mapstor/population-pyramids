e65848ab5b1b19ad19c0f0f4444b4b7d33cf0cc7
Marko Visic <moving.data.systems@gmail.com>
2025-11-02 21:49:46 +0100

    Test basic Slovenia route

---
+++ src/app/slovenia-test/page.tsx
@@ -0,0 +1,10 @@
+export const dynamic = 'force-dynamic'
+
+export default function SloveniaTest() {
+  return (
+    <div className="container mx-auto px-4 py-8">
+      <h1>Slovenia Test</h1>
+      <p>This is a simple test page for Slovenia</p>
+    </div>
+  );
+}
